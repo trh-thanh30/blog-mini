d5e92cd8933453f045bc1c11ba5c4f914b5b9c81
trh-thanh30 <tranhuuthanhcp@gmail.com>
2024-08-07 16:30:59 +0700

    success page client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,20 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Signin from "./assets/pages/Signin";
+import Home from "./assets/pages/Home";
+import About from "./assets/pages/About";
+import Signup from "./assets/pages/Signup";
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+
+          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/sign-up" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
